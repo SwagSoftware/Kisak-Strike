b1cacea07c29164e43a71a13a229e72ad9ba8010
LWSS <bbchallenger100@gmail.com>
2020-06-01 15:19:22 -0700

    allow mat_postprocess_enable changes. 0 by default.

--- game/client/viewpostprocess.cpp
+++ game/client/viewpostprocess.cpp
@@ -2480,7 +2480,7 @@ static ConVar r_queued_post_processing( "r_queued_post_processing", "0" );
 // This has really marginal effects, but 4x1 does seem vaguely better for post-processing
 static ConVar mat_postprocess_x( "mat_postprocess_x", "4" );
 static ConVar mat_postprocess_y( "mat_postprocess_y", "1" );
-static ConVar mat_postprocess_enable( "mat_postprocess_enable", "1", FCVAR_CHEAT );
+static ConVar mat_postprocess_enable( "mat_postprocess_enable", "0" );
 
 bool DoEnginePostProcessing( int x, int y, int w, int h, bool bFlashlightIsOn, bool bPostVGui )
 {
